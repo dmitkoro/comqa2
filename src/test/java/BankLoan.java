745efb150a5bd4898816a57793aaebddb9673814
dmitkoro <dmitrijs.korostiks@tele2.com>
2018-10-09 11:17:10 +0300

    Test for QA2 , Lesson2

--- src/test/java/BankLoan.java
+++ src/test/java/BankLoan.java
@@ -1,7 +1,32 @@
-public class BankLoan {
+import org.junit.jupiter.api.Test;
 
-    private void testCase (){
-        System.out.println("test");
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+    public class BankLoan {
+
+    private String text;
+
+    @Test
+    public void OurFirstTest() {
+
+
+        Integer loan = 30000;
+
+        Integer firstperiod = loan * 10 / 100 * 10;
+        Integer secondperiod = loan * 8 / 100 * 10;
+        Integer thirdperiod = loan * 6 / 100 * 10;
+        Integer totalamount = loan + firstperiod + secondperiod + thirdperiod;
+
+        Integer ExpectedTotalAmount = 102000;
+
+
+        assertEquals(ExpectedTotalAmount,totalamount,"Wrong sum !");
+        text = "Correct sum : ";
+        System.out.println(text + totalamount);
+
+
+
     }
 
 
