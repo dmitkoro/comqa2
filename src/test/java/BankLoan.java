8a856568cbe818f07c2d43f68bf2d47f72e18884
dmitkoro <dmitrijs.korostiks@tele2.com>
2018-10-09 16:47:02 +0300

    BankLoan with BigDecimals

--- src/test/java/BankLoan.java
+++ src/test/java/BankLoan.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-    public class BankLoan {
+public class BankLoan {
 
     private String text;
 
@@ -13,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
     public void OurFirstTest() {
 
 
-        BigDecimal loan = new BigDecimal (30000);
+        BigDecimal loan = new BigDecimal (250000);
 
         BigDecimal decade = new BigDecimal(10);
         BigDecimal firstPercent = new BigDecimal(0.1);
@@ -24,13 +27,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         BigDecimal secondPeriod = loan.multiply(secondPercent).multiply(decade);
         BigDecimal thirdPeriod = loan.multiply(thirdPercent).multiply(decade);
 
-        BigDecimal totalAmount = loan.add(firstPeriod).add(secondPeriod).add(thirdPeriod);
+        BigDecimal totalAmount = loan.add(firstPeriod).add(secondPeriod).add(thirdPeriod).round(new MathContext(15, RoundingMode.HALF_UP));
+        System.out.println("Expected sum is : " + totalAmount);
 
 
-        BigDecimal expectedTotalAmount = new BigDecimal (102000);
+
+        BigDecimal expectedTotalAmount = new BigDecimal (850000);
 
 
-        assertEquals(expectedTotalAmount,totalAmount,"Wrong sum !");
+
+         assertTrue(expectedTotalAmount.compareTo(totalAmount)==0);
+
         text = "Correct sum : ";
         System.out.println(text + totalAmount);
 
