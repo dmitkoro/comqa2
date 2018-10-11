45db2de5ebaad6759d26e5f0878a17d12d7eea61
dmitkoro <dmitrijs.korostiks@tele2.com>
2018-10-11 10:43:43 +0300

    Test for QA2 , Lesson2

--- src/test/java/BankLoan.java
+++ src/test/java/BankLoan.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-    public class BankLoan {
+public class BankLoan {
 
     private String text;
 
@@ -12,19 +12,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
         Integer loan = 30000;
-
-        Integer firstperiod = loan * 10 / 100 * 10;
-        Integer secondperiod = loan * 8 / 100 * 10;
-        Integer thirdperiod = loan * 6 / 100 * 10;
-        Integer totalamount = loan + firstperiod + secondperiod + thirdperiod;
-
         Integer ExpectedTotalAmount = 102000;
 
 
-        assertEquals(ExpectedTotalAmount,totalamount,"Wrong sum !");
+        Integer decade = 10;
+        Integer firstperiod = loan * 10 / 100 * decade;
+        Integer secondperiod = loan * 8 / 100 * decade;
+        Integer thirdperiod = loan * 6 / 100 * decade;
+        Integer totalamount = loan + firstperiod + secondperiod + thirdperiod;
+
+
+        assertEquals(ExpectedTotalAmount, totalamount, "Wrong sum !");
         text = "Correct sum : ";
         System.out.println(text + totalamount);
-
 
 
     }
