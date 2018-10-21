9a862ae2ae642f43a56414e66ed954953c653d3b
dmitkoro <dmitrijs.korostiks@tele2.com>
2018-10-21 18:34:59 +0300

    Test for QA2 , Lesson4 work in class

--- src/test/java/Lesson4WorkInClass.java
+++ src/test/java/Lesson4WorkInClass.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,50 +7,50 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Lesson4WorkInClass {
 
     private final By ARTICLE_TITLE_DESKTOP = By.xpath(".//h3[@class='top2012-title']/a[1]");
 
     private final String HOME_PAGE_DESKTOP = "http://rus.delfi.lv";
 
-    private int   articlesCount = 5;
+    private int articlesCount = 5;
 
 
-
-        @Test
+    @Test
 
     public void articlesVerifying() {
 
-            System.setProperty("webdriver.gecko.driver", "/home/dmitkoro/Documents/geckodriver");
-            WebDriver driver = new FirefoxDriver();
-            driver.get(HOME_PAGE_DESKTOP);
-            driver.manage().window().maximize();
+        System.setProperty("webdriver.gecko.driver", "/home/dmitkoro/Documents/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+        driver.get(HOME_PAGE_DESKTOP);
+        driver.manage().window().maximize();
 
 
-            List<WebElement> delfiList = driver.findElements((ARTICLE_TITLE_DESKTOP));
-            List<String> newListDelfi = new ArrayList<String>();
-            for (int i = 0; i < articlesCount; i++) {
+        List<WebElement> delfiList = driver.findElements((ARTICLE_TITLE_DESKTOP));
+        List<String> newListDelfi = new ArrayList<String>();
+        for (int i = 0; i < articlesCount; i++) {
 
-                newListDelfi.add(delfiList.get(i).getText());
-
-
-            }
-
-            System.out.println(newListDelfi);
-
-            List<String> customArticles = new ArrayList<String>();
-
-            customArticles.add("Из первых уст: 8 наблюдений о разнице жизни в Риге и Санкт-Петербурге");
-            customArticles.add("Троицкий: Латвия и Эстония ошиблись, обращаясь с русскоязычными жителями высокомерно");
-            customArticles.add("В Даугавпилсской думе фактически развалилась коалиция; \"Согласие\" возвращает влияние");
-            customArticles.add("В Латвии возникла нехватка вакцин для взрослых против дифтерии");
-            customArticles.add("Трамп заявил о выходе из договора с Россией о ракетах средней и малой дальности");
-
-            Assertions.assertEquals(newListDelfi,customArticles,"Articles is not match !!!");
+            newListDelfi.add(delfiList.get(i).getText());
 
 
+        }
 
-            driver.close();
+        System.out.println(newListDelfi);
+
+        List<String> customArticles = new ArrayList<String>();
+
+        customArticles.add("Из первых уст: 8 наблюдений о разнице жизни в Риге и Санкт-Петербурге");
+        customArticles.add("Троицкий: Латвия и Эстония ошиблись, обращаясь с русскоязычными жителями высокомерно");
+        customArticles.add("В Даугавпилсской думе фактически развалилась коалиция; \"Согласие\" возвращает влияние");
+        customArticles.add("В Латвии возникла нехватка вакцин для взрослых против дифтерии");
+        customArticles.add("Трамп заявил о выходе из договора с Россией о ракетах средней и малой дальности");
+
+        assertEquals(newListDelfi, customArticles, "Articles is not match !!!");
+
+
+        driver.close();
 
     }
 
