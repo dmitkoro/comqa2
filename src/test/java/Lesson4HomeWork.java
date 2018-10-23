b3f59fc1755112dbd2d115c651e1de43fcbf69a0
dmitkoro <dmitrijs.korostiks@tele2.com>
2018-10-23 10:54:21 +0300

    Changes in Lesson4HomeWork

--- src/test/java/Lesson4HomeWork.java
+++ src/test/java/Lesson4HomeWork.java
@@ -1,13 +1,22 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Lesson4HomeWork {
 
-    private final By ARTICLE_TITLE_DESKTOP = By.xpath(".//h3[@class='top2012-title']/a[1]");
-    private final By ARTICLE_TITLE_MOBILE = By.xpath(".//a[@class='md-scrollpos']");
+    private final By CATEGORIES = By.xpath("//ul[@class='sf-menu clearfix menu-content sf-js-enabled sf-arrows']/li/a");
+    private final By DRESSES = By.xpath("//div[@id='subcategories']//a[@title='Dresses']");
+    private final By COLOR_ORANGE = By.id("layered_id_attribute_group_13");
     private final String HOME_PAGE_DESKTOP = "http://automationpractice.com";
+    private WebDriver driver;
+    private final int CATEGORIES_COUNTER = 3;
 
 
     @Test
@@ -16,12 +25,42 @@ public class Lesson4HomeWork {
 
 
         System.setProperty("webdriver.gecko.driver", "/home/dmitkoro/Documents/geckodriver");
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         driver.get(HOME_PAGE_DESKTOP);
         driver.manage().window().maximize();
 
-        driver.close();
+
+        List<WebElement> siteCategories = driver.findElements(CATEGORIES);
+
+        Assertions.assertFalse(siteCategories.isEmpty(),"siteCategories elements were not found !");
+
+
+        for (int i = 0; i < CATEGORIES_COUNTER; i++) {
+
+            if (siteCategories.get(i).getText().equalsIgnoreCase("WOMEN")) {
+
+                siteCategories.get(i).click();
+                break;
+
+            }
+
+        }
+
+        WebElement siteDresses = driver.findElement(DRESSES);
+        siteDresses.click();
+
+
+        WebElement orangeColor = driver.findElement(COLOR_ORANGE);
+        orangeColor.click();
+
+
     }
+
+//    @AfterEach
+//    private void CloseBrowser() {
+//        driver.close();
+//
+//    }
 
 }
 
